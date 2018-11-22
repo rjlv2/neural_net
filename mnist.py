21ab321f608ffc1b6f1fb7fedfaf4914cf80fcae
Ryan Voo <ryanvoo95@gmail.com>
2018-11-22 19:55:26 +0000

    Changed weight to sample, need to implement different cost function

--- mnist.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import matplotlib.pyplot as plt
-import numpy as np
-
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
